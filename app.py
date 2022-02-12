de5424d2474c198329534f95df1e2c45ad4cfc9d
Lucas Freeman <lucaswfreeman@gmail.com>
2022-02-12 17:26:16 -0600

    tutorial mode clean up, creating DateInformation class

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
+# external libraries
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from os import getenv
+
+# internal libraries
+from date_information import DateInformation
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = "hello"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = getenv("FLASK_APP_SECRET_KEY")
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 
-class users(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
+# class users(db.Model):
+#     _id = db.Column("id", db.Integer, primary_key=True)
+#     name = db.Column(db.String(100))
+#     email = db.Column(db.String(100))
 
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
+#     def __init__(self, name, email):
+#         self.name = name
+#         self.email = email
 
 
 @app.route("/")
@@ -26,68 +34,68 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        session.permanent = True
-        user = request.form["nm"]
-        session["user"] = user
-        found_user = users.query.filter_by(name=user).first()
-        if found_user:
-            session["email"] = found_user.email
-        else:
-            usr = users(user, "")
-            db.session.add(usr)
-            db.session.commit()
+# @app.route("/login", methods=["POST", "GET"])
+# def login():
+#     if request.method == "POST":
+#         session.permanent = True
+#         user = request.form["nm"]
+#         session["user"] = user
+#         found_user = users.query.filter_by(name=user).first()
+#         if found_user:
+#             session["email"] = found_user.email
+#         else:
+#             usr = users(user, "")
+#             db.session.add(usr)
+#             db.session.commit()
 
-        flash("Login Successful!")
-        return redirect(url_for("user"))
-    else:
-        if "user" in session:
-            flash("Already Logged in!")
-            return redirect(url_for("user"))
-        return render_template("login.html")
-
-
-@app.route("/view_users")
-def view():
-    return render_template("view.html", values=users.query.all())
+#         flash("Login Successful!")
+#         return redirect(url_for("user"))
+#     else:
+#         if "user" in session:
+#             flash("Already Logged in!")
+#             return redirect(url_for("user"))
+#         return render_template("login.html")
 
 
-@app.route("/user", methods=["POST", "GET"])
-def user():
-    email = None
-    if "user" in session:
-        user = session["user"]
-
-        if request.method == "POST":
-            email = request.form["email"]
-            session["email"] = email
-            found_user = users.query.filter_by(name=user).first()
-            found_user.email = email
-            db.session.commit()
-            flash("Email has been saved!")
-        else:
-            if "email" in session:
-                email = session["email"]
-        return render_template("user.html", email=email)
-    else:
-        flash("You are not logged in!")
-        return redirect(url_for("login"))
+# @app.route("/view_users")
+# def view():
+#     return render_template("view.html", values=users.query.all())
 
 
-@app.route("/logout")
-def logout():
-    if "user" in session:
-        user = session["user"]
-        flash(f"You have been logged out, {user}!", "info")
-    else:
-        flash("You aren't logged in, you silly goose!")
-    session.pop("user", None)
-    session.pop("email", None)
-    return redirect(url_for("login"))
+# @app.route("/user", methods=["POST", "GET"])
+# def user():
+#     email = None
+#     if "user" in session:
+#         user = session["user"]
+
+#         if request.method == "POST":
+#             email = request.form["email"]
+#             session["email"] = email
+#             found_user = users.query.filter_by(name=user).first()
+#             found_user.email = email
+#             db.session.commit()
+#             flash("Email has been saved!")
+#         else:
+#             if "email" in session:
+#                 email = session["email"]
+#         return render_template("user.html", email=email)
+#     else:
+#         flash("You are not logged in!")
+#         return redirect(url_for("login"))
+
+
+# @app.route("/logout")
+# def logout():
+#     if "user" in session:
+#         user = session["user"]
+#         flash(f"You have been logged out, {user}!", "info")
+#     else:
+#         flash("You aren't logged in, you silly goose!")
+#     session.pop("user", None)
+#     session.pop("email", None)
+#     return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
-    db.create_all()
+    # db.create_all()
     app.run(debug=True)
---
+++ date_information.py
@@ -0,0 +1,140 @@
+from datetime import date, timedelta
+import requests
+import json
+from dotenv import load_dotenv
+from os import getenv
+
+
+class DateInformation:
+    def __init__(self, date: date):
+        # TODO: Include support for BC dates using
+        self.date = date
+        self.date_data = {
+            "events": [],
+            "other_years_events": [],
+            "births": [],
+            "birthdays": [],
+            "deaths": [],
+            "currency_rates": [],
+        }
+
+    # These wiki functions grabs the historical information tied to the day and month.
+    # Because it is pulling back way more information than what is specific to
+    # the day/month/year, I'm going to use the extra data to include a "things
+    # that happened on other years on this day" section to the app.
+
+    # TODO: Remove the redundancy in wiki_events and wiki_births by creating a function that handles the seperation of current years and other years
+
+    def wiki_events(self):
+        r = requests.get(
+            f"https://byabbe.se/on-this-day/{self.date.month}/{self.date.day}/events.json"
+        )
+        if not r.ok:
+            # raise Exception("ERROR: Count not fetch results")
+            self.date_data["events"] = None
+            self.date_data["other_years_events"] = None
+            # TODO: put some sort of error flag here
+        else:
+            data = r.json()
+            events = []
+            other_years_events = []
+            for event in data["events"]:
+                # I am taking into account if the API adds "AD" to the beginning or end of the year.
+                if event["year"] in (
+                    str(self.date.year),
+                    f"{str(self.date.year)} AD",
+                    f"AD {str(self.date.year)}",
+                ):
+                    events.append(event)
+                # because the user can't enter BC dates, I don't want them displayed in the other_years_events list.
+                elif "BC" in (event["year"][-2:], event["year"][2:]):
+                    continue
+                else:
+                    other_years_events.append(event)
+            if events:
+                self.date_data["events"] = events
+            else:
+                self.date_data["events"] = None
+            if other_years_events:
+                self.date_data["other_years_events"] = other_years_events
+            else:
+                self.date_data["other_years_events"] = None
+
+    def wiki_births(self):
+        r = requests.get(
+            f"https://byabbe.se/on-this-day/{self.date.month}/{self.date.day}/births.json"
+        )
+        if not r.ok:
+            # raise Exception("ERROR: Count not fetch results")
+            self.date_data["births"] = None
+            self.date_data["birthdays"] = None
+            # TODO: put some sort of error flag here
+        else:
+            data = r.json()
+            births = []
+            birthdays = []
+            for birth in data["births"]:
+                # I am taking into account if the API adds "AD" to the beginning or end of the year.
+                if birth["year"] in (
+                    str(self.date.year),
+                    f"{str(self.date.year)} AD",
+                    f"AD {str(self.date.year)}",
+                ):
+                    births.append(birth)
+                # Because the user can't enter BC dates, I don't want them displayed in the birthdays list.
+                elif "BC" in (birth["year"][-2:], birth["year"][2:]):
+                    continue
+                # I only want to populate this with people born before the input date so that I can have a "birthdays" section
+                # Since I've already dealt with BC dates, I don't need to consider them here.
+                elif int(birth["year"]) < self.date.year:
+                    birthdays.append(birth)
+            if births:
+                self.date_data["births"] = births
+            else:
+                self.date_data["births"] = None
+            if birthdays:
+                self.date_data["birthdays"] = birthdays
+            else:
+                self.date_data["birthdays"] = None
+
+    def wiki_deaths(self):
+        r = requests.get(
+            f"https://byabbe.se/on-this-day/{self.date.month}/{self.date.day}/deaths.json"
+        )
+        if not r.ok:
+            # raise Exception("ERROR: Count not fetch results")
+            self.date_data["deaths"] = None
+            # TODO: put some sort of error flag here
+        else:
+            data = r.json()
+            deaths = []
+            for death in data["deaths"]:
+                # I am taking into account if the API adds "AD" to the beginning or end of the year.
+                if death["year"] in (
+                    str(self.date.year),
+                    f"{str(self.date.year)} AD",
+                    f"AD {str(self.date.year)}",
+                ):
+                    deaths.append(death)
+                # Because the user can't enter BC dates, I don't want them displayed in the birthdays list.
+                elif "BC" in (death["year"][-2:], death["year"][2:]):
+                    continue
+            if deaths:
+                self.date_data["deaths"] = deaths
+            else:
+                self.date_data["deaths"] = None
+            pass
+
+        # self.date_data["events"] = dictData
+
+    # def wiki_births(self)
+
+    # print(data)
+
+
+test_date = date(year=2020, month=2, day=29)
+date_information = DateInformation(test_date)
+date_information.wiki_events()
+date_information.wiki_births()
+date_information.wiki_deaths()
+pass
