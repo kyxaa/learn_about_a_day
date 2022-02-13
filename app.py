07c1431a0a7b4364cd39ff75abaa5aaa01e07f16
Lucas Freeman <lucaswfreeman@gmail.com>
2022-02-13 09:20:59 -0600

    adjusted when we pull data from the api, cruft cleanup, visual tweaks

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 # external libraries
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from flask_session import Session
-
+from datetime import date
 from flask_wtf import FlaskForm
-from wtforms import DateField, validators, SubmitField
-from datetime import timedelta
+from wtforms import DateField, SubmitField
+from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from os import getenv
-import jsonpickle
+
+
+def flash_future_dates(form, field):
+    if field.data > date.today():
+        flash("We can't possibly know the future, you silly goose...but here you go!")
+        return
 
 
 class DateInput(FlaskForm):
-    date = DateField("Day to Learn About", validators=(validators.DataRequired(),))
+    date = DateField(
+        "Day to Learn About",
+        validators=(
+            DataRequired(),
+            flash_future_dates,
+        ),
+    )
     submit = SubmitField("Submit")
 
 
@@ -22,10 +32,7 @@ from date_information import DateInformation
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = getenv("FLASK_APP_SECRET_KEY")
-Session(app)
 
 
 @app.route("/")
@@ -37,12 +44,15 @@ def index():
 def date_input():
     form = DateInput()
     if form.validate_on_submit():
-        date_information = DateInformation(form.date.data)
-        session["date_information"] = date_information.toJSON()
+        date_for_session = [
+            form.date.data.month,
+            form.date.data.day,
+            form.date.data.year,
+        ]
+        # date_information = DateInformation(form.date.data)
+        session["date_for_session"] = date_for_session
         return redirect(url_for("view"))
-    if request.method == "POST":
-        pass
-    else:
+    if request.method == "GET":
         return render_template("date_input.html", form=form)
 
 
@@ -53,8 +63,13 @@ def todo():
 
 @app.route("/view/", methods=["GET", "POST"])
 def view():
-    date_information = jsonpickle.decode(session["date_information"])
-    return render_template("view.html", date=date_information.date)
+    date_from_session = date(
+        month=session["date_for_session"][0],
+        day=session["date_for_session"][1],
+        year=session["date_for_session"][2],
+    )
+    date_information = DateInformation(date_from_session)
+    return render_template("view.html", date_information=date_information)
 
 
 if __name__ == "__main__":
