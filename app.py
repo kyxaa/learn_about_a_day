461796668577d65b65fcb6922b2a7ca765458841
Lucas Freeman <lucaswfreeman@gmail.com>
2022-02-14 15:21:01 -0600

    added a default value to date field, extra validation

--- app.py
+++ app.py
@@ -20,8 +20,10 @@ def flash_future_dates(form, field):
 
 
 class DateInput(FlaskForm):
+
     date = DateField(
         "Day to Learn About",
+        default=date(month=11, day=19, year=1863),
         validators=(
             DataRequired(),
             flash_future_dates,
@@ -43,37 +45,56 @@ def index():
 
 @app.route("/date_input/", methods=["POST", "GET"])
 def date_input():
+    # # Logic to create a random date for the populating of the default value in the form
+    # start_date = date(1, 1, 1)
+    # end_date = date.today()
+
+    # time_between_dates = end_date - start_date
+    # days_between_dates = time_between_dates.days
+    # random_number_of_days = random.randrange(days_between_dates)
+    # random_date = start_date + timedelta(days=random_number_of_days)
+
     form = DateInput()
     if form.validate_on_submit():
-        date_for_session = [
-            form.date.data.month,
-            form.date.data.day,
-            form.date.data.year,
-        ]
-        # date_information = DateInformation(form.date.data)
-        session["date_for_session"] = date_for_session
+        try:
+            date_for_session = [
+                form.date.data.month,
+                form.date.data.day,
+                form.date.data.year,
+            ]
+            # date_information = DateInformation(form.date.data)
+            session["date_for_session"] = date_for_session
+        except:
+            flash("We weren't able to parse out your data. Try again.")
+            return redirect(url_for("date_input"))
         return redirect(url_for("output"))
     if request.method == "GET":
         return render_template("date_input.html", form=form)
 
 
-@app.route("/output/", methods=["GET", "POST"])
+@app.route("/output/", methods=["GET"])
 def output():
     if session.get("date_for_session"):
-        date_from_session = date(
-            month=session["date_for_session"][0],
-            day=session["date_for_session"][1],
-            year=session["date_for_session"][2],
-        )
-        date_information = DateInformation(date_from_session)
-        session.pop("date_for_session")
-        return render_template(
-            "output.html",
-            date_information=date_information,
-            today=date.today(),
-            formatted_full_date=date_information.date.strftime("%A, %B %d, %Y"),
-            formatted_month_day=date_information.date.strftime("%B %d"),
-        )
+        try:
+            date_from_session = date(
+                month=session["date_for_session"][0],
+                day=session["date_for_session"][1],
+                year=session["date_for_session"][2],
+            )
+            date_information = DateInformation(date_from_session)
+            session.pop("date_for_session")
+            return render_template(
+                "output.html",
+                date_information=date_information,
+                today=date.today(),
+                formatted_full_date=date_information.date.strftime("%A, %B %d, %Y"),
+                formatted_month_day=date_information.date.strftime("%B %d"),
+            )
+        except:
+            flash(
+                "We tried to take the date value and pop it into the backend, but something went wrong. Try again."
+            )
+            return redirect(url_for("date_input"))
     else:
         return redirect(url_for("date_input"))
 
